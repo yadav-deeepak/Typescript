7fe9190e224892c806e84278e8daeef5a5eaf377
Deepak Yadav <deepakyadav11767@gmail.com>
2025-07-20 13:25:28 +0530

    Decorators

--- decorators/dist/app.js
+++ decorators/dist/app.js
@@ -1,7 +1,4 @@
 "use strict";
-/**
- * Understanding Decorators
- */
 var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
     function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
     var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
@@ -40,17 +37,40 @@ var __setFunctionName = (this && this.__setFunctionName) || function (f, name, p
     if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
     return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
 };
-function Logger(target) {
-    console.log('Logging...');
-    console.log(target);
+/**
+ * Understanding Decorators
+ */
+function LoggerDecorator(logMsg) {
+    console.log('Logger factory function');
+    function Logger(target) {
+        console.log("Logger Decorator called");
+        //  console.log(logMsg);
+        //  console.log(target);
+    }
+    return Logger;
 }
 // Here it will take target parameter and here target is this User class 
 // Here we are specifying the type of target as a function because as we know that the class we create in js uses the constructor function syntax behind the scene 
 // User class behind the scene is a function 
 // Note: In order to use decorators we need to go to tsconfig.json file and uncomment experimentalDecorators: true 
 // Decorator will be called first so first logging and then user constructor is called will be printed
+function Template(template, elementId) {
+    console.log('Template factory function');
+    return function (target) {
+        console.log("Template Decorator called");
+        const u = new target();
+        const container = document.getElementById(elementId);
+        if (container) {
+            container.innerHTML = template;
+            const h2 = container.querySelector('h2');
+            if (h2) {
+                h2.textContent = 'Hello Mr. ' + u.name;
+            }
+        }
+    };
+}
 let User = (() => {
-    let _classDecorators = [Logger];
+    let _classDecorators = [LoggerDecorator('This is a custom decoartor...'), Template('<h2>Dynamic Header</h2>', 'container')];
     let _classDescriptor;
     let _classExtraInitializers = [];
     let _classThis;
@@ -72,4 +92,4 @@ let User = (() => {
     return User = _classThis;
 })();
 // Even if we dont create an object then also the decorator will be called whenever this class will be encountered the decorator will be called .
-const u = new User();
+// const u = new User();
--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -1,10 +1,14 @@
 /**
  * Understanding Decorators 
  */
-
-function Logger (target: Function){
-    console.log('Logging...');
-    console.log(target);
+function LoggerDecorator(logMsg: string){
+    console.log('Logger factory function');
+    function Logger (target: Function){
+        console.log("Logger Decorator called");
+        //  console.log(logMsg);
+        //  console.log(target);
+    }
+    return Logger;
 }
 // Here it will take target parameter and here target is this User class 
 // Here we are specifying the type of target as a function because as we know that the class we create in js uses the constructor function syntax behind the scene 
@@ -12,7 +16,24 @@ function Logger (target: Function){
 // Note: In order to use decorators we need to go to tsconfig.json file and uncomment experimentalDecorators: true 
 // Decorator will be called first so first logging and then user constructor is called will be printed
 
-@Logger
+function Template(template: string, elementId: string){
+    console.log('Template factory function');
+    return function(target: any){
+        console.log("Template Decorator called");
+        const u = new target();
+        const container = document.getElementById(elementId);
+        if(container){
+            container.innerHTML = template;
+            const h2 = container.querySelector('h2');
+            if(h2){
+                h2.textContent = 'Hello Mr. ' + u.name;
+            }
+        }
+    }
+}
+
+@LoggerDecorator('This is a custom decoartor...')
+@Template('<h2>Dynamic Header</h2>', 'container')
 class User{
     name: string = 'John';
     age: number = 23;
@@ -23,4 +44,4 @@ class User{
 }
 // Even if we dont create an object then also the decorator will be called whenever this class will be encountered the decorator will be called .
 
-const u = new User();
+// const u = new User();
